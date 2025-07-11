418d1c7682e8dc0a06767a8ab9181c72024f64f6
Saman Aminoroaya <aminoroayas@gmail.com>
2025-07-11 15:52:57 +0330

    feat: implement city search component with dropdown functionality and update styles

--- src/app/shared/city-search/city-search.ts
+++ src/app/shared/city-search/city-search.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-city-search',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './city-search.html',
-  styleUrl: './city-search.scss'
+  styleUrl: './city-search.scss',
 })
 export class CitySearch {
+  dropDownShow = signal<boolean>(false);
+  cityList = signal<any[]>([]);
+  searchTerm!: string;
 
+  searchCity(e: Event) {
+    console.log(e);
+  }
 }
