f089bb47efab1c2634e443f8fbd386e1bc515e91
Saman Aminoroaya <aminoroayas@gmail.com>
2025-07-11 14:38:38 +0330

    update routing to include HomePageComponent and rename HomePage class to HomePageComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './features/home/home-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: HomePageComponent }];
--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
-export class HomePage {
+export class HomePageComponent {
 
 }
