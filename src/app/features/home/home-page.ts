10a179b2c9edba1104c40bbf592b72779bc73b36
Saman Aminoroaya <aminoroayas@gmail.com>
2025-07-11 14:49:41 +0330

    refactor: update home page to include weather components and set up environment variables

--- src/app/features/home/home-page.ts
+++ src/app/features/home/home-page.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { CitySearch } from "../../shared/city-search/city-search";
+import { ForecastList } from "../../shared/forecast-list/forecast-list";
+import { WeatherCard } from "../../shared/weather-card/weather-card";
+import { UnitToggle } from "../../shared/unit-toggle/unit-toggle";
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [CitySearch, ForecastList, WeatherCard, UnitToggle],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,2 @@
+export const API_KEY: string = '6c4e9e3225149fab79b7d14416c49333';
+export const API_BASE_URL: string = 'https://api.openweathermap.org/data/2.5/';
