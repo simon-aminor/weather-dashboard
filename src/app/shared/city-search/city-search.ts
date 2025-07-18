5d764947f8503f4d1f5676fd6ba1590d14bec50c
Saman Aminoroaya <aminoroayas@gmail.com>
2025-07-18 23:20:25 +0330

    refactor: enhance city search component with dropdown functionality and styling improvements

--- src/app/shared/city-search/city-search.ts
+++ src/app/shared/city-search/city-search.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, ElementRef, signal, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-city-search',
@@ -8,9 +9,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './city-search.scss',
 })
 export class CitySearch {
-  dropDownShow = signal<boolean>(false);
-  cityList = signal<any[]>([]);
-  searchTerm!: string;
+  protected searchBox =
+    viewChild.required<ElementRef<HTMLInputElement>>('searchBox');
+  protected showDropdown = signal<boolean>(false);
+  protected cityList = signal<any[]>([
+    'Yazd',
+    'Mashad',
+    'Shiraz',
+    'Isfahan',
+    'Tehran',
+  ]);
+  protected searchTerm!: string;
+
+  ngAfterViewInit(): void {
+    fromEvent(this.searchBox().nativeElement, 'focus').subscribe(() => {
+      this.showDropdown.set(true);
+    });
+    fromEvent(this.searchBox().nativeElement, 'blur').subscribe(() => {
+      setTimeout(() => this.showDropdown.set(false), 200);
+    });
+  }
 
   searchCity(e: Event) {
     console.log(e);
