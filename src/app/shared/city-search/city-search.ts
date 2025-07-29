512e101860b806aede40df32291d60e924fced1f
Saman Aminoroaya <aminoroayas@gmail.com>
2025-07-30 00:55:46 +0330

    init value ti filtered zones to show on init view of search box

--- src/app/shared/city-search/city-search.ts
+++ src/app/shared/city-search/city-search.ts
@@ -13,31 +13,16 @@ export class CitySearch {
     viewChild.required<ElementRef<HTMLInputElement>>('searchBox');
   protected showDropdown = signal<boolean>(false);
   protected searchTerm = signal<string | null>(null);
-  protected filteredZones = signal<Zone[]>([]);
-  protected zoneList = signal<Zone[]>([
-    { city: 'Tokyo', country: 'Japan' },
-    { city: 'Paris', country: 'France' },
-    { city: 'Sydney', country: 'Australia' },
-    { city: 'Rio de Janeiro', country: 'Brazil' },
-    { city: 'Cairo', country: 'Egypt' },
-    { city: 'Toronto', country: 'Canada' },
-    { city: 'Berlin', country: 'Germany' },
-    { city: 'Bangkok', country: 'Thailand' },
-    { city: 'Nairobi', country: 'Kenya' },
-    { city: 'Rome', country: 'Italy' },
-    { city: 'New York', country: 'USA' },
-    { city: 'Moscow', country: 'Russia' },
-    { city: 'Dubai', country: 'UAE' },
-    { city: 'Buenos Aires', country: 'Argentina' },
-    { city: 'Seoul', country: 'South Korea' },
-  ]);
+  protected filteredZones = signal<Zone[]>(Zones);
+  protected zoneList = signal<Zone[]>(Zones);
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     const inputEl = this.searchBox().nativeElement;
 
     fromEvent(this.searchBox().nativeElement, 'focus').subscribe(() => {
       this.showDropdown.set(true);
     });
+
     fromEvent(this.searchBox().nativeElement, 'blur').subscribe(() => {
       setTimeout(() => this.showDropdown.set(false), 200);
     });
@@ -66,3 +51,20 @@ interface Zone {
   city: string;
   country: string;
 }
+const Zones = [
+  { city: 'Tokyo', country: 'Japan' },
+  { city: 'Paris', country: 'France' },
+  { city: 'Sydney', country: 'Australia' },
+  { city: 'Rio de Janeiro', country: 'Brazil' },
+  { city: 'Cairo', country: 'Egypt' },
+  { city: 'Toronto', country: 'Canada' },
+  { city: 'Berlin', country: 'Germany' },
+  { city: 'Bangkok', country: 'Thailand' },
+  { city: 'Nairobi', country: 'Kenya' },
+  { city: 'Rome', country: 'Italy' },
+  { city: 'New York', country: 'USA' },
+  { city: 'Moscow', country: 'Russia' },
+  { city: 'Dubai', country: 'UAE' },
+  { city: 'Buenos Aires', country: 'Argentina' },
+  { city: 'Seoul', country: 'South Korea' },
+];
