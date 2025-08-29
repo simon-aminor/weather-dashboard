674234f7107f095313c48dae3a02e60aacf6fd53
Saman Aminoroaya <aminoroayas@gmail.com>
2025-08-29 10:18:06 +0330

    reverse to tailwind 3

--- src/app/shared/weather-card/weather-card.ts
+++ src/app/shared/weather-card/weather-card.ts
@@ -14,3 +14,25 @@ export class WeatherCard {
     console.log(this.weatherData());
   });
 }
+// module.exports = {
+//   content: ["./src/**/*.{html,ts,scss}"],
+//   theme: {
+//     extend: {
+//       keyframes: {
+//         "slide-fade-in": {
+//           "0%": { opacity: "0", transform: "translateY(-10px)" },
+//           "100%": { opacity: "1", transform: "translateY(0)" },
+//         },
+//         "slide-fade-out": {
+//           "0%": { opacity: "1", transform: "translateY(0)" },
+//           "100%": { opacity: "0", transform: "translateY(-10px)" },
+//         },
+//       },
+//       animation: {
+//         "slide-fade": "slide-fade-in 0.3s ease forwards",
+//         "slide-fade-reverse": "slide-fade-out 0.3s ease forwards",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
